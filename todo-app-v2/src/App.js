60096b5a0da60221086b34ce88f02a354e96c35e
Jonny Vorn Soth <janysoth@gmail.com>
2024-10-21 05:18:22 -0500

    Create markup for the neccessary components for the app.

--- todo-app-v2/src/App.js
+++ todo-app-v2/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import AddNewTodo from './components/AddNewTodo';
+import Calendar from './components/Calendar';
+import EditToDo from './components/EditTodo';
+import Header from './components/Header';
+import Main from './components/Main';
+import Projects from './components/Projects';
+import Todos from './components/Todos';
+import User from './components/User';
 
 function App() {
   return (
-    <div>
-      Todo App
+    <div className='App'>
+      <Header>
+        <User />
+        <AddNewTodo />
+        <Calendar />
+        <Projects />
+      </Header>
+      <Main>
+        <Todos />
+        <EditToDo />
+      </Main>
     </div>
   );
 }
---
+++ todo-app-v2/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ({ children }) => {
+  return (
+    <div className="Header">
+      {children}
+    </div>
+  );
+};
+
+export default Header;
