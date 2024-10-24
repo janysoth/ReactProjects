17204872232ab4fb2daa7fe7610f1934e86cda00
Jonny Vorn Soth <janysoth@gmail.com>
2024-10-24 03:06:02 -0500

    Create and style the markup for Modal component to use in AddNewTodo.

--- todo-app-v2/src/components/AddNewTodo.js
+++ todo-app-v2/src/components/AddNewTodo.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from './Modal';
 
 const AddNewTodo = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className='AddNewTodo'>
-      AddNewTodo
+      <div className="btn">
+        <button onClick={() => setShowModal(true)}>
+          + New Todo
+        </button>
+      </div>
+
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div>
+          Hello World
+          <button onClick={() => setShowModal(false)}>
+            Hide
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
