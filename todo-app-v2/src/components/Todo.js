1b742e9e12247853a5e56d34e9b833ce990b9023
Jonny Vorn Soth <janysoth@gmail.com>
2024-11-05 01:55:36 -0600

    Create a markup for Todo component.

--- todo-app-v2/src/components/Todo.js
+++ todo-app-v2/src/components/Todo.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ArrowClockwise, CheckCircleFill, Circle, Trash } from 'react-bootstrap-icons';
 
 const Todo = ({ todo }) => {
+  const [hover, setHover] = useState(false);
+
   return (
     <div className='Todo'>
-      {todo.text}
+      <div
+        className="todo-container"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div className="check-todo">
+          {
+            todo.checked ?
+              <span className="checked">
+                <CheckCircleFill color='#bebebe' />
+              </span>
+              :
+              <span className="uncheck">
+                <Circle color={todo.color} />
+              </span>
+          }
+        </div>
+
+        <div className="text">
+          <p style={{ color: todo.checked ? '#bebebe' : '#00000' }}>
+            {todo.text}
+          </p>
+
+          <span>{todo.time} - {todo.project}</span>
+
+          <div className={`line ${todo.checked ? 'line-through' : ''}`} />
+        </div>
+
+        <div className="add-to-next-day">
+          {
+            todo.checked &&
+            <span>
+              <ArrowClockwise />
+            </span>
+          }
+        </div>
+
+        <div className="delete-todo">
+          {
+            (hover || todo.checked) &&
+            <span>
+              <Trash />
+            </span>
+          }
+        </div>
+      </div>
     </div>
   );
 };
--- todo-app-v2/src/components/Todos.js
+++ todo-app-v2/src/components/Todos.js
@@ -8,17 +8,17 @@ const Todos = () => {
   const todos = [
     {
       id: 'd54sd4',
-      text: "Go for a run",
+      text: "go for a run",
       time: "10:00 AM",
       date: "12/05/2024",
       day: "6",
       checked: false,
-      color: '#000000',
+      color: '#00ff00',
       project: 'personal'
     },
     {
       id: 'd54fdf',
-      text: "Meeting",
+      text: "meeting friends",
       time: "09:00 AM",
       date: "12/09/2024",
       day: "1",
@@ -39,7 +39,7 @@ const Todos = () => {
           selectedProject === "next 7 days" ?
             <Next7Days todos={todos} /> :
             todos.map(todo =>
-              <Todo todo={todo} key={todo.key} />
+              <Todo todo={todo} key={todo.id} />
             )
         }
       </div>
