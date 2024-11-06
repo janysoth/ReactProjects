3fccdfe61afd1353aa73cf9abd55af11a65d3493
Jonny Vorn Soth <janysoth@gmail.com>
2024-11-06 03:58:14 -0600

    Create TodoContext to access global datas (selectedProject and setSelectedProject).

--- todo-app-v2/src/index.js
+++ todo-app-v2/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { TodoContextProvider } from './context';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <TodoContextProvider>
     <App />
-  </React.StrictMode>
+  </TodoContextProvider>
 );
 
