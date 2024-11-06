435005d1caaa258789a79fe89386a64f91bafe4f
Jonny Vorn Soth <janysoth@gmail.com>
2024-11-06 03:38:38 -0600

    Create EditTodo component.

--- todo-app-v2/src/components/AddNewTodo.js
+++ todo-app-v2/src/components/AddNewTodo.js
@@ -1,17 +1,22 @@
-
-import DateFnsUtils from '@date-io/date-fns';
-// import MomentUtils from '@date-io/moment';
-import { DatePicker, MuiPickersUtilsProvider, TimePicker } from '@material-ui/pickers';
 import React, { useState } from 'react';
-import { Bell, Calendar, Clock, Palette, X } from 'react-bootstrap-icons';
 import Modal from './Modal';
+import TodoForm from './TodoForm';
 
-const AddNewTodo = () => {
+function AddNewTodo() {
   const [showModal, setShowModal] = useState(false);
   const [text, setText] = useState('');
   const [day, setDay] = useState(new Date());
   const [time, setTime] = useState(new Date());
 
+  const projects = [
+    { id: 1, name: "personal", numOfTodos: 0 },
+    { id: 2, name: "work", numOfTodos: 1 },
+    { id: 3, name: "other", numOfTodos: 2 }
+  ];
+
+  function handleSubmit(e) {
+
+  }
 
   return (
     <div className='AddNewTodo'>
@@ -20,77 +25,23 @@ const AddNewTodo = () => {
           + New Todo
         </button>
       </div>
-
       <Modal showModal={showModal} setShowModal={setShowModal}>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <form>
-            <div className="text">
-              <h3>Add New Todo!</h3>
-              <input
-                type='text'
-                value={text}
-                onChange={e => setText(e.target.value)}
-                placeholder='Enter your Todo here...'
-                autoFocus
-              />
-            </div>
-
-            <div className="remind">
-              <Bell />
-              <p>Remind Me!</p>
-            </div>
-
-            <div className="pick-day">
-              <div className="title">
-                <Calendar />
-                <p>Choose a Day</p>
-              </div>
-              <DatePicker
-                value={day}
-                onChange={day => setDay(day)}
-              />
-            </div>
-
-            <div className="pick-time">
-              <div className="title">
-                <Clock />
-                <p>Choose Time</p>
-              </div>
-              <TimePicker
-                value={time}
-                onChange={time => setTime(time)}
-              />
-            </div>
-
-            <div className="pick-project">
-              <div className="title">
-                <Palette />
-                <p>Choose a Prject</p>
-              </div>
-
-              <div className="projects">
-                <div className="project active">
-                  Personal
-                </div>
-
-                <div className="project">
-                  Work
-                </div>
-              </div>
-            </div>
-
-            <div className="cancel" onClick={() => setShowModal(false)}>
-              <X size='40' />
-            </div>
-
-            <div className="confirm">
-              <button>+ Add Todo</button>
-            </div>
-          </form>
-        </MuiPickersUtilsProvider>
+        <TodoForm
+          handleSubmit={handleSubmit}
+          heading='Add New Todo'
+          text={text}
+          setText={setText}
+          day={day}
+          setDay={setDay}
+          time={time}
+          setTime={setTime}
+          projects={projects}
+          showButtons={true}
+          setShowModal={setShowModal}
+        />
       </Modal>
     </div>
   );
-};
+}
 
 export default AddNewTodo;
--- todo-app-v2/src/components/EditTodo.js
+++ todo-app-v2/src/components/EditTodo.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
 
 const EditTodo = () => {
+
+  const [text, setText] = useState();
+  const [day, setDay] = useState();
+  const [time, setTime] = useState();
+
+  const projects = [
+    { id: 1, name: "personal", numOfTodos: 0 },
+    { id: 2, name: "work", numOfTodos: 1 },
+    { id: 3, name: "other", numOfTodos: 2 }
+  ];
+
+  const handleSubmit = () => {
+
+  };
+
   return (
     <div className='EditTodo'>
-      EditTodo
+      <div className="header">
+        Edit Todo
+      </div>
+      <div className="container">
+        <TodoForm
+          handleSubmit={handleSubmit}
+          text={text}
+          setText={setText}
+          day={day}
+          setDay={setDay}
+          time={time}
+          setTime={setTime}
+          projects={projects}
+        />
+      </div>
     </div>
   );
 };
