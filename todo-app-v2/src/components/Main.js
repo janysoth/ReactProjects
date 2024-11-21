a7c48214a0b92003974f5c8b942fd861297f99f2
Jonny Vorn Soth <janysoth@gmail.com>
2024-11-21 03:07:48 -0600

    Remove the EditTodo component when the user click on anywhere in the Main component.

--- todo-app-v2/src/components/Main.js
+++ todo-app-v2/src/components/Main.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
+
+import { TodoContext } from '../context';
 
 const Main = ({ children }) => {
+  // CONTEXT
+  const { setSelectedTodo } = useContext(TodoContext);
+
+  // REF
+  const mainRef = useRef();
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => document.removeEventListener('click', handleClick);
+  });
+
+  const handleClick = e => {
+    if (e.target === mainRef.current)
+      setSelectedTodo(undefined);
+  };
   return (
-    <div className="Main">
+    <div className="Main" ref={mainRef}>
       {children}
     </div>
   );
