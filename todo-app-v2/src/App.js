87107387671ddb132a70e5087151fbd7aad1bdc3
Jonny Vorn Soth <janysoth@gmail.com>
2024-11-27 03:32:06 -0600

    Create a markup for LoginPage.

--- todo-app-v2/src/App.js
+++ todo-app-v2/src/App.js
@@ -1,8 +1,11 @@
+// import { useAuthState } from "react-firebase-hooks/auth";
+
+import { auth } from "./firebase";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
-  const user = true;
+  const user = false;
   return (
     <div>
       {!user ? <LoginPage /> : <HomePage />}
--- todo-app-v2/src/pages/LoginPage.js
+++ todo-app-v2/src/pages/LoginPage.js
@@ -1,8 +1,25 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react';
 
+import { auth, provider } from "../firebase";
+
 function LoginPage() {
+  const signInUser = (e) => {
+    e.preventDefault();
+
+    signInWithPopup(auth, provider).catch((err) => alert(err.message));
+
+  };
+
   return (
-    <div>LoginPage</div>
+    <div className='LoginPage'>
+      <div className='container'>
+        <h1>My Todo App</h1>
+        <button onClick={signInUser} className='button'>
+          Sign in with Google
+        </button>
+      </div>
+    </div>
   );
 }
 
