a8c88e87d9e090f7fa4d65f5d3355c47987ca2ca
Jonny Vorn Soth <janysoth@gmail.com>
2024-12-12 02:48:08 -0600

    Create React app called notes-app.

---
+++ notes-app/frontend/notes-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/dashboard' exact element={<Home />} />
+      <Route path='/login' exact element={<Login />} />
+      <Route path='/signup' exact element={<SignUp />} />
+    </Routes>
+  </Router>
+);
+
+const App = () => {
+  return (
+    <div>
+      {routes}
+    </div>
+  );
+};
+
+export default App;
---
+++ notes-app/frontend/notes-app/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar";
+
+const Login = () => {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+};
+
+export default Login;
