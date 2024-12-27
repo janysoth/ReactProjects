6828754b5e1073351bd7d2eaeeb5c2af06402f46
Jonny Vorn Soth <janysoth@gmail.com>
2024-12-27 12:15:01 -0600

    Add CORS middleware and update package-lock.json and package.json files to reflect new dependency.

--- notes-app-v2/backend/server.js
+++ notes-app-v2/backend/server.js
@@ -1,5 +1,33 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import connect from "./db/connect.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+// middleware
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const server = async () => {
-  console.log("Server is running");
+  try {
+    await connect();
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to start server...", error.message);
+    process.exit(1);
+  }
 };
 
 server();
