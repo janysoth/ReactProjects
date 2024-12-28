0225c71ac5daf825e8e36f41540043d50589182c
Jonny Vorn Soth <janysoth@gmail.com>
2024-12-28 11:11:53 +0100

    Add cookie-parser dependency and configure it in server.js

--- notes-app-v2/backend/server.js
+++ notes-app-v2/backend/server.js
@@ -1,6 +1,8 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+
 import connect from "./db/connect.js";
 
 dotenv.config();
@@ -16,6 +18,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 const server = async () => {
   try {
