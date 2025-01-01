66a5d62bb024445975c4eb17500eb871efe776f1
Jonny Vorn Soth <janysoth@gmail.com>
2025-01-01 23:54:16 +0100

    Add user login functionality to userController.js and update userRoutes.js to include login endpoint.

--- notes-app-v2/backend/src/controllers/auth/userController.js
+++ notes-app-v2/backend/src/controllers/auth/userController.js
@@ -56,3 +56,60 @@ export const registerUser = asyncHandler(async (req, res) => {
     res.status(400).json({ message: "Invalid User Data." });
   }
 });
+
+// User login
+export const loginUser = asyncHandler(async (req, res) => {
+  // get email and password from req.body
+  const { email, password } = req.body;
+
+  // Validation
+  if (!email || !password) {
+    // 400 Bad Request
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (!userExists) {
+    return res.status(404).json({ message: "User not found, sign up!" });
+  }
+
+  // Check id the password match the hashed password in the database
+  const isMatch = await bcrypt.compare(password, userExists.password);
+
+  if (!isMatch) {
+    // 400 Bad Request
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  // Generate token with user id
+  const token = generateToken(userExists._id);
+
+  if (userExists && isMatch) {
+    const { _id, name, email, role, photo, bio, isVerified } = userExists;
+
+    // Set the token in the cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      sameSite: "none", // cross-site access --> allow all third-party cookies
+      secure: true,
+    });
+
+    // Send back the user and token in the response to the client
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      role,
+      photo,
+      bio,
+      isVerified,
+      token,
+    });
+  } else {
+    res.status(400).json({ message: "Invalid email or password" });
+  }
+});
