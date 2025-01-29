3984e2deb373fba8277114bf7598b7e9117eaf81
Jonny Vorn Soth <janysoth@gmail.com>
2025-01-29 04:02:18 -0600

    Remove Home page functionality and added moment package.

--- notes-app-v2/client/app/layout.tsx
+++ notes-app-v2/client/app/layout.tsx
@@ -1,7 +1,8 @@
-import UserProvider from "@/providers/UserProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+
+import UserProvider from "@/providers/UserProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -28,8 +29,11 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
-        <Toaster position="top-center" />
-        <UserProvider>{children}</UserProvider>
+
+        <UserProvider>
+          <Toaster position="top-center" />
+          {children}
+        </UserProvider>
       </body>
     </html>
   );
