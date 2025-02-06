f8902849a8e277c8df668d44d2875aaa8690cf18
Jonny Vorn Soth <janysoth@gmail.com>
2025-02-06 02:26:46 -0600

    Refactor login page and useUserRedirect hook to handle user authentication.

--- notes-app-v2/client/app/login/page.tsx
+++ notes-app-v2/client/app/login/page.tsx
@@ -1,12 +1,22 @@
+"use client";
+import { useUserContext } from '@/context/userContext';
+import useRedirect from '@/hooks/useUserRedirect';
 import React from 'react';
 import LoginForm from "../components/auth/LoginForm";
 
-function page() {
+function Page() {
+  const { user } = useUserContext();
+
+  if (user) {
+    useRedirect('/'); // Directly call the hook if user is logged in
+    return null; // Prevent rendering the login page
+  }
+
   return (
     <div className='auth-page w-full h-full flex justify-center items-center'>
       <LoginForm />
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
--- notes-app-v2/client/app/page.tsx
+++ notes-app-v2/client/app/page.tsx
@@ -5,7 +5,6 @@ import useRedirect from "@/hooks/useUserRedirect";
 export default function Home() {
   useRedirect("/login");
 
-
   return (
     <main></main>
   );
