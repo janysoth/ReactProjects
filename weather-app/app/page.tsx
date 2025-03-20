15562ad184add4e436576cb88846e0ebcaff322f
Jonny Vorn Soth <janysoth@gmail.com>
2025-03-20 02:48:18 -0500

    Create Wind.tsx to display in page.tsx

--- weather-app/app/page.tsx
+++ weather-app/app/page.tsx
@@ -5,16 +5,17 @@ import FiveDayForecast from "./Components/FiveDayForecast";
 import Navbar from "./Components/Navbar";
 import Sunset from "./Components/Sunset";
 import Temperature from "./Components/Temperature";
+import Wind from "./Components/Wind";
 
 export default function Home() {
   return (
-    <main className="mx-[1rem] lg:mx-[2rem] xl:[6rem] 2xl:[16rem] m-auto">
+    <main className="mx-[1rem] lg:mx-[2rem] xl:mx-[6rem] 2xl:mx-[16rem] m-auto">
       <Navbar />
 
       <div className="pb-4 flex flex-col gap-4 md:flex-row">
         <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
           <Temperature />
-          <FiveDayForecast />
+          {/* <FiveDayForecast /> */}
         </div>
 
         <div className="flex flex-col w-full">
@@ -23,6 +24,7 @@ export default function Home() {
           >
             <AirPollution />
             <Sunset />
+            <Wind />
           </div>
         </div>
       </div>
