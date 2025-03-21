58285f288e38749393673c836f54e995da7e4fb5
Jonny Vorn Soth <janysoth@gmail.com>
2025-03-21 03:13:32 -0500

    Create DailyForecast.tsx to display DailyForecast in page.tsx

--- weather-app/app/page.tsx
+++ weather-app/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AirPollution from "./Components/AirPollution";
+import DailyForecast from "./Components/DailyForecast";
 import FiveDayForecast from "./Components/FiveDayForecast";
 import Navbar from "./Components/Navbar";
 import Sunset from "./Components/Sunset";
@@ -15,16 +16,17 @@ export default function Home() {
       <div className="pb-4 flex flex-col gap-4 md:flex-row">
         <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
           <Temperature />
-          {/* <FiveDayForecast /> */}
+          <FiveDayForecast />
         </div>
 
         <div className="flex flex-col w-full">
           <div
-            className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4"
+            className="instruments grid  gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4"
           >
             <AirPollution />
             <Sunset />
             <Wind />
+            <DailyForecast />
           </div>
         </div>
       </div>
