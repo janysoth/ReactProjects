723b868617c009c6c239aa7d255475387c79f09a
Jonny Vorn Soth <janysoth@gmail.com>
2025-04-08 02:23:34 -0500

    feat: add routing and authentication redirection to the app
    - Updated package.json to include new dependencies: @tailwindcss/vite, axios, emoji-picker-react, moment, react-hot-toast, react-icons, react-router-dom, and recharts.
    - Implemented routing in App.jsx with routes for Login, SignUp, Dashboard, Income, and Expense pages.
    - Added Root component for authentication check and redirection based on token presence in localStorage.
    - Enhanced index.css with base styles for html and body elements.

--- expense-tracker-app/frontend/expense-tracker-app/src/App.jsx
+++ expense-tracker-app/frontend/expense-tracker-app/src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Expense from './pages/Dashboard/Expense';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
 export default App;
+
+const Root = () => {
+  // Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  // Redirect to Dashboard if authenticated, otherwise to login 
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+};
