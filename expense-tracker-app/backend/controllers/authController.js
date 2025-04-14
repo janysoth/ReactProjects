9a51fd2b13543a45dd79d90406e1e9f15c542b80
Jonny Vorn Soth <janysoth@gmail.com>
2025-04-13 22:45:21 -0500

    feat: add user authentication middleware and implement getUserInfo route

--- expense-tracker-app/backend/controllers/authController.js
+++ expense-tracker-app/backend/controllers/authController.js
@@ -77,4 +77,19 @@ exports.loginUser = async (req, res) => {
 };
 
 // Get User Info
-exports.getUserInfo = async (req, res) => { };
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error in getting user info backend: ",
+      error: error.message
+    });
+  }
+};
