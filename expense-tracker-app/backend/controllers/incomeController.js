7f9fd5c0d6a8095652e1dccdfae61e56ecf7b413
Jonny Vorn Soth <janysoth@gmail.com>
2025-04-16 01:39:59 -0500

    feat: enhance income management with delete and download functionality, and improve error messages

--- expense-tracker-app/backend/controllers/incomeController.js
+++ expense-tracker-app/backend/controllers/incomeController.js
@@ -1,4 +1,5 @@
-const User = require('../models/User');
+const xlsx = require('xlsx');
+
 const Income = require('../models/Income');
 
 // Add Income Source
@@ -30,17 +31,73 @@ exports.addIncome = async (req, res) => {
 
   } catch (error) {
     res.status(500).json({
-      message: "Error in adding income source: ",
+      message: "Error in adding income source on the backend: ",
       error: error.message
     });
   }
 };
 
 // Get All Income Sources
-exports.getAllIncome = async (req, res) => { };
+exports.getAllIncome = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+    res.json(income);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error in fetching income sources on the backend: ",
+      error: error.message
+    });
+
+  }
+};
 
 // Delete Income Source
-exports.deleteIncome = async (req, res) => { };
+exports.deleteIncome = async (req, res) => {
+  try {
+    const deletedIncome = await Income.findByIdAndDelete(req.params.id);
+
+    if (!deletedIncome) {
+      return res.status(404).json({ message: "Income source not found." });
+    }
+
+    res.json({ message: "Income source deleted successfully." });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error in deleting income source on the backend.",
+      error: error.message
+    });
+  }
+};
 
 // Download Income Sources as Excel
-exports.downloadIncomeExcel = async (req, res) => { };
+exports.downloadIncomeExcel = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+
+    // Prepare data for Excel
+    const data = income.map((item) => ({
+      Source: item.source,
+      Amount: item.amount,
+      Date: item.date.toISOString().split("T")[0], // item.date
+    }));
+
+    // Convert to Excel format
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+
+    xlsx.utils.book_append_sheet(wb, ws, "Income");
+    xlsx.writeFile(wb, "income_details.xlsx");
+
+    res.download('income_details.xlsx');
+  } catch (error) {
+    res.status(500).json({
+      message: "Error in downloading income sources as excel on the backend: ",
+      error: error.message
+    });
+
+  }
+};
