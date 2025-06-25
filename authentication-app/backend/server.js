afe4ae9429355cf1424ebfd243385b806c393f22
Jonny Vorn Soth <janysoth@gmail.com>
2025-06-25 02:45:50 -0500

    feat: initialize authentication app with frontend and backend setup
    - Added frontend with React, Vite, and basic styling.
    - Created main entry point for React application.
    - Configured Vite for development environment.
    - Established package.json with scripts for running server and client concurrently.
    - Included necessary devDependencies for development.

---
+++ authentication-app/backend/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const connectDB = require("./config/db");
+
+// Declare the routes
+// const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true // ✅ if using tokens or cookies
+  })
+);
+
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Use the routes declare above
+// app.use("/api/v1/auth", authRoutes);
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
